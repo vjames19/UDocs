1c72ca150db7c33ae62d4d892ead5fc055ec4ace
ebreijo <eduardo.breijo@upr.edu>
2014-09-28 10:10:29 -0400

    Added share form and controller

--- app/scripts/share/share-ctrl.js
+++ app/scripts/share/share-ctrl.js
@@ -1,3 +1,19 @@
-app.controller('ShareCtrl', function() {
+app.controller('ShareCtrl', function(Data) {
+	this.disableSubmit = true;
+	this.callbacks = {
+    success: function(files) {
+      console.log('the success callback', files);
+      this.disableSubmit = false;
+      Data.createDocument(files).then(function(response) {
+        console.log('promise', response);
+        this.disableSubmit = true;
+      });
+    }
+  };
+	this.submitForm = function() {
+		var departmentName = this.department;
+		var courseName = this.course;
+		var professorName = this.professor;
 
+	}
 });
